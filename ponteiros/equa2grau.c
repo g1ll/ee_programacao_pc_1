a27edee969f3642e749c3b03d019d95459a4af4f
Gill <gillvelleda@gmail.com>
2019-11-25 19:58:18 -0300

    Exemplos passagem por referencia

---
+++ ponteiros/equa2grau.c
@@ -0,0 +1,37 @@
+#include<stdio.h>
+#include<math.h>
+
+int calcEqSegundoGrau(float a,float b, float c,float *x1,float *x2);
+
+int main(void){
+    float r1,r2,a,b,c;
+
+    printf("\nCoeficientes (a b c):\n");
+    scanf("%f %f %f",&a,&b,&c);
+
+    if(calcEqSegundoGrau(a,b,c,&r1,&r2)==1)
+        printf("\nNao possui raizes reais!");
+    else
+        printf("\nRaizes:\n\tr1: %f\n\tr2: %f",r1,r2);
+    
+    printf("\n\n\n");
+    getchar();
+    getchar();
+    return 0;
+}
+
+int calcEqSegundoGrau(float a,float b, float c,float *x1,float *x2){
+    
+    int cod = 0;
+    float dl=0;
+
+    dl  = b*b -4*a*c;
+    if(dl>0){
+        *x1 = (-b+sqrt(dl))/2*a;
+        *x2 = (-b-sqrt(dl))/2*a;
+    }else{
+        cod = 1;
+    }
+    
+    return cod;
+}
