ba95d77058a5ba626d29428cd414e51c54a252b2
Gill <gillvelleda@gmail.com>
2019-11-27 13:41:38 -0300

    Exemplo criação de arquivo

--- arquivos/newfile.c
+++ arquivos/newfile.c
@@ -11,11 +11,12 @@ int main() {
   if(file!=NULL){
     printf("\nNome a ser gravado: ");
     scanf("\n%s",cont);
-    fprintf(file,"Nome: %s",cont);
+    fprintf(file,"\nNome: %s",cont);
     printf("Informe a idade: ");
     scanf("\n%s",cont);
     fprintf(file,"\nIdade: %s\n",cont);
     printf("\nGravado!\n\n");
+	fclose(file);
   }else{	
     printf("\nErro %s\n\n",fname);
   }
